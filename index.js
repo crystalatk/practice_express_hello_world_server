a8ec657c1f71b33c1c1dffd5ff7b009a5b0ba3f4
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-01-26 16:21:01 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const HTTP = require('http');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3333;
+
+const express = require('express');
+const app = express();
+
+const SERVER = HTTP.createServer(app);
+
+SERVER.listen(PORT, HOSTNAME, function() {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+});
+
+const rootController = require('./routes/index');
+const catsController = require('./routes/cats');
+const dogsController = require('./routes/dogs');
+const catsAndDogsController = require('./routes/cats_and_dogs');
+
+app.use('/', rootController);
+app.use('/cats', catsController);
+app.use('/dogs', dogsController);
+app.use('/catsanddogs', catsAndDogsController);
